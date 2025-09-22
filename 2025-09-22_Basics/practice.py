8508babed8e7af674141248affb6a1c669a3190f
Jaydeep Sonawane <jaydeepsonaw@gmail.com>
2025-09-22 16:19:04 +0530

    Py

--- 2025-09-22_Basics/practice.py
+++ 2025-09-22_Basics/practice.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+print("Hello, World!")
+
+# Data in Python
+print(type("Hello, World!"))
+print(type(2.99))
+print(type(299))
+print(type("jaydeep")) 
+
+# Simple Maths
+print(7676+444)
+print(7079709/444)
+print(4534*23)
+print(9090-90)
+print(9090**9)
+
+
+
+
